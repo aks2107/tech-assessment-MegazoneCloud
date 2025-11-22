af8d7167248d76eeea4f22042fb2ecc5b49e7053
aswar <aks2107@rit.edu>
2025-11-22 15:24:39 -0500

    Add logic for checking input files and creating output file

--- data-engineering/script.py
+++ data-engineering/script.py
@@ -7,3 +7,23 @@ MEMBERINFO_FILE = os.path.join(DATA_DIR, 'memberInfo.csv') # This allows the pat
 MEMBERPAIDINFO_FILE = os.path.join(DATA_DIR, 'memberPaidInfo.csv')
 OUTPUT_FILE = os.path.join(OUTPUT_DIR, 'cleanData.csv')
 
+def check_directories():
+    """
+    This function checks to make sure the required directories are present. 
+    If an output directory is missing it gets created.
+    If the input files and directory is not present then the program terminates alerting the user that something is missing.
+    """
+    if not os.path.exists(OUTPUT_DIR):
+        os.makedirs(OUTPUT_DIR)
+    if not os.path.exists(DATA_DIR):
+        print("The data directory is missing and the script will terminate.")
+        exit(1)
+    if not os.path.exists(MEMBERINFO_FILE):
+        print("memberinfo.csv is missing and the script will terminate.")
+        exit(1)
+    if not os.path.exists(MEMBERPAIDINFO_FILE):
+         print("memberPaidInfo.csv is missing and the script will terminate.")
+         exit(1)
+
+
+
